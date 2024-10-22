1c4a0d97be8543e60d19f2541a02c324e70f178e
Alan Alvarez <alvarezalanfranco@gmail.com>
2024-10-22 02:05:01 -0300

    Se comienza a realizar trabajo practico nro 4, sobre Streams y metodos por referencia.

---
+++ Trabajo Practico 4/src/mains/Main.java
@@ -0,0 +1,71 @@
+package mains;
+
+import java.util.Comparator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import models.Persona;
+import models.Rectangulo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TP nro 4, metodos por referencia con objeto definido e objeto indefinidop
+		//Ejemplo de uso, creacion de lista con 50 rectangulkos distintos.
+		var listaRectangulos = Stream.generate(Rectangulo::getRandom).limit(50).toList();
+		//hoja 2 
+		//1) crear una lista de a partir de un stream formado con los dias de la semana:
+		var listaDias = Stream.of("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo").toList();
+		
+		//2) crear una lista con 60 personas aleatorias en una variable llamada ListaPersonas
+		var listaPersonas = Stream.generate(Persona::getRandom).limit(60).toList();
+		
+		//Hoja 2 parte 2
+		//1) Mostrar todas las personas
+		Consumer<Persona> mostrarPersona = s -> System.out.println(s);
+		System.out.println("Lista de personas:");
+		listaPersonas.stream().forEach(mostrarPersona);
+		
+		//2) Mostrar todos los rectangulos
+		Consumer<Rectangulo> mostrarRectangulo = s -> System.out.println(s);
+		System.out.println("Lista de rectangulos: ");
+		listaRectangulos.stream().forEach(mostrarRectangulo);
+		
+		//3) Duplicar la base de todos los rectangulos
+		Consumer<Rectangulo> duplicarBaseR = s -> s.setBase(s.getBase() * 2);
+		listaRectangulos.stream().forEach(duplicarBaseR);
+		listaRectangulos.forEach(mostrarRectangulo);
+		
+		//Hoja 3
+		//1)Evaluar si todas las personas son mayores de 20 años
+		boolean mayoresDeVeinte = listaPersonas.stream().allMatch(p -> p.getEdad() > 20 );
+		System.out.println(mayoresDeVeinte ? "Todos son mayores de 20" : "Hay menores de 20");
+		
+		//2)Evaluar si existe al menos una persona de más de 1.95 cm.
+		boolean existenMayoresDe195 = listaPersonas.stream().anyMatch(p -> p.getEstatura() > 1.95);
+		System.out.println(existenMayoresDe195 ? "Hay personas mayores de 1,95" : "Todos son menores de 1,95");
+		
+		//3) Evaluar si no existe ninguna que su nombre empiece con R.
+		Predicate<Persona> conR = s -> s.getNombre().startsWith("R");
+		boolean personasConR = listaPersonas.stream().noneMatch(conR);
+		System.out.println(personasConR ? "No hay personas con R" : "Existen personas con R");
+
+		//4) Evaluar si todos los rectángulos tienen el perímetro menor que el área.
+		Predicate<Rectangulo> perimetroMenorArea = s -> s.getPerimetro() < s.getArea();
+		boolean rectanguloPerMenArea = listaRectangulos.stream().allMatch(perimetroMenorArea);
+		System.out.println(rectanguloPerMenArea ? "Todos los rectangulos tienen perimetro menor que area." : "Hay rectangulos con perimetro mayor al area.");
+		
+		//5) Evaluar si existe al menos un rectángulo donde el perímetro sea el triple de la altura.
+		Predicate<Rectangulo> perimetroTripleAltura = r -> r.getPerimetro() == r.getAltura() * 3;
+		boolean rectanguloPerTriAlt = listaRectangulos.stream().anyMatch(perimetroTripleAltura);
+		System.out.println( rectanguloPerTriAlt ? "Hay al menos un rectangulo con perimetro del triple de la altura" : "No hay rectangulos con perimetro del triple de la altura.");
+		
+		//6) Evaluar si no existe ningún rectángulo donde el la altura sea más del doble de la base
+		Predicate<Rectangulo> alturaDobleBase = r -> r.getAltura() > r.getBase() * 2;
+		boolean rectanguloAlturaDobleBase = listaRectangulos.stream().noneMatch(alturaDobleBase);
+		System.out.println(rectanguloAlturaDobleBase ? "No hay rectangulos con altura mayor al doble de la base." : "Hay rectangulos con altura mayor al doble de la base.");
+
+	}
+
+}
--- Trabajo Practico 4/src/models/Persona.java
+++ Trabajo Practico 4/src/models/Persona.java
@@ -1,17 +1,36 @@
 package models;
 
+import utils.Randomizador;
+
 public class Persona {
+
 	private String nombre;
+	private String apellido;
 	private int edad;
 	private double peso;
 	private double estatura;
 
-	public Persona(String nombre, int edad, double peso, double estatura) {
+	public static Persona getRandom() {
+		Randomizador r = new Randomizador();
+		return new Persona(r.getNombre(), r.getApellido(), r.getInt(15, 80), r.getDouble(45, 120), r.getDouble(1.3, 2.1));
+
+	}
+
+	public Persona(String nombre, String apellido, int edad, double peso, double estatura) {
 		super();
 		this.nombre = nombre;
+		this.apellido = apellido;
 		this.edad = edad;
 		this.peso = peso;
 		this.estatura = estatura;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
 	}
 
 	public String getNombre() {
@@ -39,10 +58,21 @@ public class Persona {
 	}
 
 	public double getEstatura() {
-		return estatura;
+		return this.estatura;
 	}
 
 	public void setEstatura(double estatura) {
 		this.estatura = estatura;
 	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", peso=" + peso
+				+ ", estatura=" + estatura + "]";
+	}
+
+	public boolean isMenor() {
+		return this.getEdad() < 18;
+	}
+
 }
